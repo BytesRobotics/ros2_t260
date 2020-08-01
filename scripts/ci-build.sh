67f5024211fc2861c89c731417bee8c827ec8d99
Michael Equi <michaelrequi@gmail.com>
2020-08-01 13:36:41 -0700

    Added source to ci scripts

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -6,4 +6,5 @@ cp -R include ~/ws/src/ros2_t260/
 cp CMakeLists.txt ~/ws/src/ros2_t260/
 cp package.xml ~/ws/src/ros2_t260/
 cd ~/ws
+source /opt/ros/$ROS_DISTRO/setup.bash
 colcon build
--- scripts/ci-test.sh
+++ scripts/ci-test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd ~/ws
+source /opt/ros/$ROS_DISTRO/setup.bash
 colcon test
 colcon test-result --verbose
