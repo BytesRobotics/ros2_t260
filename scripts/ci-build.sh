7f68d7e4126d5c73fb27046d0508b6b84499b6a5
Michael Equi <michaelrequi@gmail.com>
2020-08-06 09:57:15 -0700

    Add compying of launch to ci-build

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -3,6 +3,7 @@
 mkdir -p ~/ws/src/ros2_t260
 cp -R src ~/ws/src/ros2_t260/
 cp -R include ~/ws/src/ros2_t260/
+cp -R launch ~/ws/src/ros2_t260/
 cp CMakeLists.txt ~/ws/src/ros2_t260/
 cp package.xml ~/ws/src/ros2_t260/
 cd ~/ws
